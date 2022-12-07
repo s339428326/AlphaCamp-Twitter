d6066efce8d9de216e79186ca099e6fe4616eada
Stanley Wong <s339428326@gmail.com>
2022-12-07 15:05:50 +0800

    feat:add PageTitle.jsx file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import './App.css';
-//
 import "./styles/main.scss";
+
+//test components
+import PageTitle from "./components/PageTitle/PageTitle";
 
 function App() {
   return (
---
+++ src/components/PageTitle/PageTitle.jsx
@@ -0,0 +1,9 @@
+const PageTitle = () => {
+  return (
+    <div>
+      <h1>PageTitle</h1>
+    </div>
+  );
+};
+
+export default PageTitle;
