8babb703ac05698064b99664f418feb2dc1211d2
Stanley Wong <s339428326@gmail.com>
2022-12-11 00:42:22 +0800

    feat: add login, register, admin pages rounter
    新增login, register, admin 三頁路由

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 // import './App.css';
 import "./styles/main.scss";
 
-//test components
-import PageTitle from "./components/PageTitle/PageTitle";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserLoginPage, RegisterPage, AdminPage } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Start</h1>
-      <button className="btn btn-primary text-white">Test</button>
-      <div className="row justify-content-center">
-        <div className="col-7">
-          <PageTitle title={"stanley"} count={12} />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserLoginPage />} />
+          <Route path="/login" element={<UserLoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
