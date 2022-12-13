854ce7702acbe1c2b7ba2085fcd0d9669624aded
LucySoCute <cutelucysu@gmail.com>
2022-12-13 17:15:01 +0800

    remove: repeated common component

--- src/pages/UserMainPage.jsx
+++ src/pages/UserMainPage.jsx
@@ -1,7 +1,7 @@
 import UserSidebar from "../components/UserSidebar/UserSidebar";
 import PageTitle from "../components/PageTitle/PageTitle";
 import MainCreateTweet from "../components/MainCreateTweet/MainCreateTweet";
-import MainTweet from "../components/MainTweet/MainTweet";
+import Tweet from "../components/Tweet/Tweet";
 import TopUser from "../components/TopUser/TopUser";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -20,7 +20,7 @@ const UserMainPage = ({ user }) => {
             <PageTitle title={"首頁"} tweetQuantity={user} />
           </div>
           <MainCreateTweet />
-          <MainTweet />
+          <Tweet />
         </Col>
         <Col xs={4} md={3}>
           <div className="sticky-top">
