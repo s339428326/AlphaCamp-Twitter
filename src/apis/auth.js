0bf07599e40377ba042878eb7e8fb4a63028f9d6
evanyang0612 <blacksheet@gmail.com>
2022-12-16 20:14:21 +0800

    feat:checkpermission, adminLogin api

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -43,3 +43,35 @@ export const register = async ({
     console.error("[Register Failed]: ", error);
   }
 };
+
+export const checkPermission = async (authToken) => {
+  try {
+    const response = await axios.get(`${AUTH_URL}/auth/test-token`, {
+      headers: {
+        Authorization: "Bearer " + authToken,
+      },
+    });
+
+    return response.status;
+  } catch (error) {
+    console.error("[Check Permission Failed]:", error);
+  }
+};
+
+export const adminLogin = async ({ account, password }) => {
+  console.log({ account, password });
+  try {
+    const { data } = await axios.post(`${AUTH_URL}/admin/signin`, {
+      account,
+      password,
+    });
+    const { token } = data.data;
+    console.log("[登入成功]", data);
+    if (token) return { success: true, ...data };
+    return data;
+  } catch (error) {
+    console.error(error);
+    console.log("[登入失敗]", error.response.data.message);
+    return { status: false };
+  }
+};
