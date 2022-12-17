a3083b9f2c0b5a1eab750c0d5bf818425aa7edd8
Stanley Wong <s339428326@gmail.com>
2022-12-17 12:38:50 +0800

    feat:UserProfile page TweetList view
    1.修正Tweet元件props
    2.Tweet 加入useMoment
    3.UserProfile page 可以讀取使用者推文清單

--- src/apis/userData.js
+++ src/apis/userData.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://rocky-sands-70657.herokuapp.com/api/";
+const BASE_URL = "https://rocky-sands-70657.herokuapp.com/api/users/";
 
 //攔截器設定
 //https://github.com/axios/axios#request-config
@@ -23,18 +23,32 @@ axiosInstance.interceptors.request.use(
 );
 
 //[U_03] get-user-profile 取得指定使用者
-//profile GET /api/users/:id
-export const getUserData = async (id) => {
-  console.log("[API URL:] :", BASE_URL + `users/${id}`);
-  if (!id) {
+//GET /api/users/:id
+export const getUserData = async (userId) => {
+  console.log("[API URL:] :", BASE_URL + `${userId}`);
+  if (!userId) {
     console.log("[使用getUserData錯誤]：請帶入使用者id");
     return;
   }
   try {
-    const res = await axiosInstance.get(BASE_URL + `users/${id}`);
+    const res = await axiosInstance.get(BASE_URL + userId);
     console.log("[取得使用者資料成功]", res.data);
     return res.data;
   } catch (error) {
     console.error("[取得使用者資料失敗]", error);
+  }
+};
+
+// [U_06] get-user-tweets 取得指定使用者的所有推文 GET /api/users/:id/tweets
+//GET /api/users/:id/tweets
+export const getUserTweets = async (userId) => {
+  if (!userId) return console.error("[getUserTweets 錯誤]:請帶入使用者id");
+  try {
+    console.log(`${BASE_URL}${userId}/tweets`);
+    const res = await axiosInstance.get(BASE_URL + userId + "/tweets");
+    console.log("[getUserTweets 成功]:", res);
+    return res.data;
+  } catch (error) {
+    console.error(error);
   }
 };
--- src/components/UserProfileTweet/UserProfileTweet.jsx
+++ src/components/UserProfileTweet/UserProfileTweet.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+//components
 import Tweet from "../Tweet/Tweet";
+//apis
+import { getUserTweets } from "../../apis/userData";
+//react-router-dom
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+//推文
+const Tweets = () => {
+  const urlUserId = useLocation().pathname.split("/")[1];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getTweets = async () => {
+      try {
+        const userTweets = await getUserTweets(urlUserId);
+        setData(userTweets.map((item) => item));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTweets();
+  }, [urlUserId]);
+  return (
+    <ul className="list-unstyled ps-0">
+      {data.map((item) => (
+        <li key={item.id}>
+          <Tweet data={item} />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const UserProfileTweet = ({ router }) => {
   return (
     <div>
-      {(router === "tweet" || router === "") && <Tweet />}
+      {(router === "tweet" || router === "") && <Tweets />}
       {router === "reply" && <p> reply</p>}
       {router === "like" && <Tweet isLike={true} />}
     </div>
